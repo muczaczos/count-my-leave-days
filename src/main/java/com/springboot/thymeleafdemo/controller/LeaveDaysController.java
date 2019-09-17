c6c0e710d23c2ab904ba409087bfa2980ce771ba
Arek Szatkowski <muczaczos@gmail.com>
2019-09-17 15:52:52 +0200

    feat: Added mechanism to updating yourLeaveDays when save or delete leave days document

--- src/main/java/com/springboot/thymeleafdemo/controller/LeaveDaysController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/LeaveDaysController.java
@@ -2,7 +2,6 @@ package com.springboot.thymeleafdemo.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springboot.thymeleafdemo.entity.Employee;
 import com.springboot.thymeleafdemo.entity.LeaveDays;
+import com.springboot.thymeleafdemo.service.EmployeeService;
 import com.springboot.thymeleafdemo.service.LeaveDaysService;
 
 @Controller
@@ -19,9 +20,11 @@ import com.springboot.thymeleafdemo.service.LeaveDaysService;
 public class LeaveDaysController {
 	
 	private LeaveDaysService leaveDaysService;
+	private EmployeeService employeeService;
 	
-	public LeaveDaysController(LeaveDaysService leaveDaysService) {
+	public LeaveDaysController(LeaveDaysService leaveDaysService, EmployeeService employeeService) {
 		this.leaveDaysService = leaveDaysService;
+		this.employeeService = employeeService;
 	}
 
 	// add mapping for "/list"
@@ -29,7 +32,7 @@ public class LeaveDaysController {
 	@GetMapping("/list")
 	public String lisLeaveDays(Model theModel) {
 		
-		// get employees from db
+		// get leave days from db
 		List<LeaveDays> theLeaveDays = leaveDaysService.findAll();
 		
 		// add to the spring model
@@ -54,10 +57,10 @@ public class LeaveDaysController {
 	public String showFormForUpdate(@RequestParam("leavedaysId") int theId,
 									Model theModel) {
 		
-		// get the employee form the service 
+		// get the leave days form the service 
 		LeaveDays theLeaveDays = leaveDaysService.findById(theId);
 		
-		// set employee as a model attribute to pre-populate the form
+		// set leave day as a model attribute to pre-populate the form
 		theModel.addAttribute("leavedays", theLeaveDays);
 		
 		// send over to our form
@@ -67,8 +70,22 @@ public class LeaveDaysController {
 	@PostMapping("/save")
 	public String saveLeaveDays(@ModelAttribute("leavedays") LeaveDays theLeaveDays) {
 		
-		// save the employee
+		// save the leave day
 		leaveDaysService.save(theLeaveDays);
+		
+		Employee employee = new Employee();
+		
+		//get employee by id from db
+		employee = employeeService.findById(theLeaveDays.getEmployee().getId());
+		
+		//get your leave days from employee
+		int yourLeaveDays = employee.getYourLeaveDays();
+		
+		//update your leave days
+		employee.setYourLeaveDays(yourLeaveDays-theLeaveDays.getLeaveDays());
+		
+		//save employee
+		employeeService.save(employee);
 		
 		//use a redirect to prevent duplicate submissions
 		return "redirect:/leavedays/list";
@@ -79,8 +96,20 @@ public class LeaveDaysController {
 	@GetMapping("/delete")
 	public String delete(@RequestParam("leavedaysId") int theId) {
 		
-		// delete the employee
+		Employee employee = new Employee();
+		
+		//get employee by id from db
+		employee = employeeService.findById(leaveDaysService.findById(theId).getEmployee().getId());
+		
+		//get your leave days from employee
+		int yourLeaveDays = employee.getYourLeaveDays();
+		
+		//update your leave days
+		employee.setYourLeaveDays(yourLeaveDays+leaveDaysService.findById(theId).getLeaveDays());
+		
+		// delete the leave day
 		leaveDaysService.deleteById(theId);
+		
 		
 		//use a redirect to /employees/list
 		return "redirect:/leavedays/list";	
