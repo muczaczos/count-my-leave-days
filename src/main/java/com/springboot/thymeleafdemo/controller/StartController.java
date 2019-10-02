76943fa5ebca318c34a2807642ffc2f3fb0d1470
Arek Szatkowski <muczaczos@gmail.com>
2019-10-02 14:19:49 +0200

    feat: added user list to start page

--- src/main/java/com/springboot/thymeleafdemo/controller/StartController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/StartController.java
@@ -1,6 +1,5 @@
 package com.springboot.thymeleafdemo.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springboot.thymeleafdemo.entity.Employee;
 import com.springboot.thymeleafdemo.entity.LeaveDays;
+import com.springboot.thymeleafdemo.entity.Users;
 import com.springboot.thymeleafdemo.service.EmployeeService;
 import com.springboot.thymeleafdemo.service.LeaveDaysService;
+import com.springboot.thymeleafdemo.service.UsersService;
 
 @Controller
 @RequestMapping("/")
@@ -21,10 +22,12 @@ public class StartController {
 
 	private LeaveDaysService leaveDayService;
 	private EmployeeService employeeService;
+	private UsersService usersService;
 
-	public StartController(LeaveDaysService theLeaveDayService, EmployeeService theEmployeeService) {
+	public StartController(LeaveDaysService theLeaveDayService, EmployeeService theEmployeeService, UsersService theUsersSerice) {
 		leaveDayService = theLeaveDayService;
 		employeeService = theEmployeeService;
+		usersService = theUsersSerice;
 
 	}
 
@@ -43,13 +46,23 @@ public class StartController {
 		try {
 			// find the employee with username from actual session
 			Employee theEmployee = employeeService.findByLogin(username);
+			
+			// find the users with username from actual session
+			List<Users> theUsers = usersService.findByUsername(username);
 
 			// find the leave days with id from theEmployee object
 			List<LeaveDays> theLeaveDays = leaveDayService.findAllById(theEmployee.getId());
+			
 
 			// add to the spring model
 			theModel.addAttribute("leavedays", theLeaveDays);
+			
+			// add to the spring model
+			theModel.addAttribute("employee", theEmployee);
 
+			// add to the spring model
+			theModel.addAttribute("users", theUsers);
+						
 			System.err.println(username + " " + theEmployee.getId());
 		} catch (NullPointerException e) {
 
