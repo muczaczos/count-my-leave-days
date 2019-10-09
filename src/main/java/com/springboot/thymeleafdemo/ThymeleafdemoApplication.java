e6c4f7d444e2bd65a319dabe3f7db143b3f3a90b
Arek Szatkowski <muczaczos@gmail.com>
2019-10-09 15:54:39 +0200

    correct: corected back links in templates and add h3 with Fistname and lastname to start page

--- src/main/java/com/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -23,6 +23,8 @@ public class ThymeleafdemoApplication extends WebMvcConfigurerAdapter {
 		
 	}
 	
+	
+	
 	   @Bean
 	   public LocaleResolver localeResolver(){
 	        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
